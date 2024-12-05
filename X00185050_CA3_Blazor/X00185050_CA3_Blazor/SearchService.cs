95403fdefb2ef70f75c713d497a0b54b7e92e347
waiho wong <x00185050@mytudublin.ie>
2024-12-05 01:12:37 +0000

    made visual changes

---
+++ X00185050_CA3_Blazor/X00185050_CA3_Blazor/SearchService.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+
+//public class SearchService
+//{
+//    public string Query { get; set; } = string.Empty;
+
+//    public event Action? OnSearchTriggered;
+
+//    public void TriggerSearch(string query)
+//    {
+//        Query = query;
+//        OnSearchTriggered?.Invoke();
+//    }
+//}
--- X00185050_CA3_Blazor/X00185050_CA3_Blazor/obj/Debug/net8.0/X00185050_CA3_Blazor.AssemblyInfo.cs
+++ X00185050_CA3_Blazor/X00185050_CA3_Blazor/obj/Debug/net8.0/X00185050_CA3_Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("X00185050_CA3_Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47d070d19a3fbe5d4f9fcfe2b8728cb5e89a705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d1dc95357d64f3b0fe18d50b79075386d167fae")]
 [assembly: System.Reflection.AssemblyProductAttribute("X00185050_CA3_Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("X00185050_CA3_Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
