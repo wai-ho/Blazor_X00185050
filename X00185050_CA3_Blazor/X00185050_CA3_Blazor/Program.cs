8414755396df3d0a9a2eaa845f70d167fc27fb3b
waiho wong <x00185050@mytudublin.ie>
2024-12-10 16:57:17 +0000

    added search and improved visuals for watchlist

--- X00185050_CA3_Blazor/X00185050_CA3_Blazor/Program.cs
+++ X00185050_CA3_Blazor/X00185050_CA3_Blazor/Program.cs
@@ -3,6 +3,10 @@ using X00185050_CA3_Blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
