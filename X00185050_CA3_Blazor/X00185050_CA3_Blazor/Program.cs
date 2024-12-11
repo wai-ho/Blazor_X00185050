8bd14a09dcaf17ba985661028e89575a812ce4d0
waiho wong <x00185050@mytudublin.ie>
2024-12-11 20:03:57 +0000

    previously created web app mistakenly and so now i created and migrated to WASM

--- X00185050_CA3_Blazor/X00185050_CA3_Blazor/Program.cs
+++ X00185050_CA3_Blazor/X00185050_CA3_Blazor/Program.cs
@@ -1,40 +1,20 @@
-using X00185050_CA3_Blazor.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using X00185050_CA3_Blazor;
 using X00185050_CA3_Blazor.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<WatchlistService>();
 
-builder.Logging.SetMinimumLevel(LogLevel.Debug);
-
-builder.Services.AddHttpClient();
-
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
-var app = builder.Build();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseDeveloperExceptionPage();
+await builder.Build().RunAsync();
 
-app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
